2acc7f26c40d6aad3295aafd2ecabe453c0eb50f
Roman Karasev <progressivenation12@gmail.com>
2024-03-23 09:58:15 +0300

    changes in the files UserDaoJDBCImpl according to the mentor's comments and delete properties file sql.properties

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,7 +8,7 @@ public class Util {
     public static Connection getConnection() {
         final String databaseURL = "jdbc:mysql://localhost:3306/kataacademy_task_1_1_4_jdbc";
         final String databaseLogin = "root";
-        final String databasePassword = "Rfhfctd@Hjvfy23";
+        final String databasePassword = "root";
 
         try {
             return DriverManager.getConnection(databaseURL, databaseLogin, databasePassword);
